f3ac921ddd7ed8209d3bcc13da44d8ce6c91191e
Paul Rose <prose2334@gmail.com>
2018-04-10 20:29:52 -0500

    add javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,9 @@
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
-
-<audio controls src="">
 
 
 $(document).ready(function () {
-
-        
+    
+            
     // var apiKey = "ce4e82bdd19cc89c06ca6cd31bedc151"
     // var searchLimit = "10"
 
@@ -22,6 +19,53 @@ $(document).ready(function () {
 
     // dump then 
 
+        
+        
+       
+
+    $('#dictionarySearch').on('click', function () {
+
+        let searchWord = $('#dictionaryWord').val().trim();
+
+        const queryURL = 'https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/entries/en/' + searchWord;
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+            headers: {
+                "Accept": "application/json", 'app_id': "c568d91b", 'app_key': "6af662dae9c81b4e19d1dcea9a92ceb3",
+            }
+            }).then(function(response) {
+        console.log(response);
+
+            
+            $('#dictionaryDefinition').text(response.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
+    
+            console.log(searchWord);
+
+            callGoogle();
+    
+    
+
+         });
+
+
+        });
+
+
+
+
+
+
+
+    // console.log($('#testDef').text());
+
+
+
+    function callGoogle () {
+
+    
+
     $.ajax({ 
         url: 'https://cors-anywhere.herokuapp.com/https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=AIzaSyDVDrFjmbXqxXuGM4oY4Gb8CAxso7ZKFOQ', 
         method: 'POST',
@@ -29,7 +73,7 @@ $(document).ready(function () {
         dataType: 'json',
         data: JSON.stringify({
             input: {
-            text: "This is a test google is pretty neat. I can read definitions. ",
+            text: $('#dictionaryDefinition').text(),
             },
             voice: {
             languageCode: 'en-US',
@@ -46,5 +90,11 @@ $(document).ready(function () {
         console.log('data:audio/mp3;base64,' + res.audioContent);
     $('audio').attr('src', 'data:audio/mp3;base64,' + res.audioContent);
 
-    });
+    })
+    // .catch(function(arg1, arg2) { console.log(arg1, arg2) })
+
+
+    };
+
+
 });
